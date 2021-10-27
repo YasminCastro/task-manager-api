b1eae9beaaddeaa254e1ddd98cebf7dab5134f62
Yasmin <yasminsdcastro@gmail.com>
2021-10-27 15:59:00 -0300

    feat: tests jest

--- tests/task.test.js
+++ tests/task.test.js
@@ -13,6 +13,9 @@ const {
 } = require("./fixtures/db");
 
 beforeEach(setupDatabase);
+
+//CREATE TASK TESTS
+
 test("Should create task for user", async () => {
   const response = await request(app)
     .post("/tasks")
@@ -29,6 +32,34 @@ test("Should create task for user", async () => {
   expect(task.completed).toEqual(false);
 });
 
+test("Should not create task with invalid description/completed", async () => {
+  await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ description: "", completed: "sim" })
+    .expect(400);
+});
+
+//UPDATE TASKS TESTS
+
+test("Should not update task with invalid description/completed", async () => {
+  await request(app)
+    .patch(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ description: "", completed: "sim" })
+    .expect(500);
+});
+
+test("Should not update other users task", async () => {
+  await request(app)
+    .patch(`/tasks/${taskOne._id}`)
+    .set("Authorizarion", `Bearer ${userTwo.tokens[0].token}`)
+    .send({ description: "wash hair", completed: true })
+    .expect(401);
+});
+
+//GET TASKST TESTS
+
 test("Should fetch tasks for UserOne ", async () => {
   const response = await request(app)
     .get("/tasks")
@@ -38,6 +69,86 @@ test("Should fetch tasks for UserOne ", async () => {
 
   expect(response.body).toHaveLength(2);
 });
+
+//FETCHING FILTERED TASKS TESTS
+
+test("Should fetch user task by id", async () => {
+  await request(app)
+    .get(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not fetch user task by id if unauthenticated", async () => {
+  await request(app).get(`/tasks/${taskOne._id}`).send().expect(401);
+});
+
+test("Should not fetch other users task by id", async () => {
+  await request(app)
+    .get(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+});
+
+test("Should fetch only completed tasks", async () => {
+  await request(app)
+    .get(`/tasks?completed=true`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should fetch only incomplete tasks", async () => {
+  await request(app)
+    .get(`/tasks?completed=false`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should sort tasks by description", async () => {
+  await request(app)
+    .get(`/tasks?sortBy=description:desc`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should sort tasks by completed", async () => {
+  await request(app)
+    .get(`/tasks?sortBy=completed:desc`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should sort tasks by createdAt", async () => {
+  await request(app)
+    .get(`/tasks?sortBy=createdAt:desc`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should sort tasks by updatedAt", async () => {
+  await request(app)
+    .get(`/tasks?sortBy=updatedAt:desc`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should fetch page of tasks", async () => {
+  await request(app)
+    .get(`/tasks?sortBy=createdAt:desc&limit=1&skip=1`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+//DELETE TASKS TESTS
 
 test("Should not let user two delete users one task", async () => {
   await request(app)
@@ -49,4 +160,16 @@ test("Should not let user two delete users one task", async () => {
   //assert the task still in the database
   const task = await Task.findById(taskOne._id);
   expect(task).not.toBeNull();
+});
+
+test("Should delete user task", async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not delete task if unauthenticated", async () => {
+  await request(app).delete(`/tasks/${taskOne._id}`).send().expect(401);
 });
--- tests/user.test.js
+++ tests/user.test.js
@@ -11,6 +11,8 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+//SINGUP TESTS
+
 test("Should signup a new user", async () => {
   const response = await request(app)
     .post("/users")
@@ -34,6 +36,15 @@ test("Should signup a new user", async () => {
   });
   expect(user.password).not.toBe("Venus123!");
 });
+
+test("Should not signup user with invalid name/email/password", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({ name: 123, email: "notEmail", password: "1234" })
+    .expect(400);
+});
+
+//LOGIN TESTS
 
 test("Should login existing user", async () => {
   const response = await request(app)
@@ -60,6 +71,8 @@ test("Should not login nonexistent user", async () => {
     .expect(400);
 });
 
+//PROFILE TESTS
+
 test("Should get profile for user", async () => {
   await request(app)
     .get("/users/me")
@@ -71,6 +84,8 @@ test("Should get profile for user", async () => {
 test("Should not get profile for unauthenticated user", async () => {
   await request(app).get("/users/me").send().expect(401);
 });
+
+//DELETE TESTS
 
 test("Should delete account for user", async () => {
   await request(app)
@@ -88,6 +103,8 @@ test("Should not delete account for ununthenticated user", async () => {
   await request(app).delete("/users/me").send().expect(401);
 });
 
+//AVATARS TESTS
+
 test("Should upload avatar image", async () => {
   await request(app)
     .post("/users/me/avatar")
@@ -98,6 +115,8 @@ test("Should upload avatar image", async () => {
   const user = await User.findById(userOneId);
   expect(user.avatar).toEqual(expect.any(Buffer));
 });
+
+//UPDATE TESTS
 
 test("Should update valid user fields", async () => {
   await request(app)
@@ -116,5 +135,17 @@ test("Shoud not update invalid user fileds", async () => {
     .patch("/users/me")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send({ address: "brasil" })
+    .expect(400);
+});
+
+test("Should not update user if unauthenticated", async () => {
+  await request(app).patch("/users/me").send({ name: "test2" }).expect(401);
+});
+
+test("Should not update user with invalid name/email/password", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ name: "", email: "", password: "" })
     .expect(400);
 });
